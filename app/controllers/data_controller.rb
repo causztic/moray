3867a3ce29149f6828755c086dba9f1c606a381f
yaojie <limyaojie93@gmail.com>
2016-04-23 00:55:09 +0800

    + require params for data

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,12 @@
 class DataController < ApplicationController
 
   def new
+    operation = Operation.new(operation_params)
+    operation.save!
+  end
+
+  def operation_params
+    params.require(:operation).permit!
   end
 
 end
