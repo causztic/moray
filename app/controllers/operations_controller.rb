826df0b7b5d38e1038244188c3baaf165281dddc
yaojie <limyaojie93@gmail.com>
2016-04-23 11:10:41 +0800

    update custom json to show all values

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -2,4 +2,11 @@ class OperationsController < ApplicationController
   def show
     @operation = Operation.find(params[:id])
   end
+
+  def custom
+    @operation =  Operation.find(params[:operation_id])
+    @catches = @operation.catches
+    render :show unless params[:show_all]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Knock::Engine => "/knock"
   resources :operations, only: [:show], defaults: { format: 'json' } do
+    get :custom
     resources :catches, only: [:show, :index], defaults: { format: 'json' }, controller: "operations/catches" do
       # resource :environment, only: [:show], defaults: { format: 'json '}, controller: "operation/catch/environment"
       resources :specimen, only: [:index], defaults: { format: 'json '}, controller: "operations/catches/specimen"
